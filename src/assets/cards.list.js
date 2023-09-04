b698060d57c0d0242f75635a394eff9b4d643e6f
Rutik <ritikrajput6351@gmail.com>
2023-09-04 21:28:45 +0530

    First Commit

---
+++ src/assets/cards.list.js
@@ -0,0 +1,312 @@
+export const list = [
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://www.superiorpoolsnc.com/wp-content/gallery/pools-and-spas/Pool_7.jpg",
+            "https://yardmasterz.com/wp-content/uploads/2017/07/amazing-backyards-1.jpg",
+            "https://tse2.mm.bing.net/th?id=OIP.biYcl3TeGV_xddf-4B_K8gHaE8&pid=Api&P=0&h=180",
+            "https://tse1.mm.bing.net/th?id=OIP.5PQ3nJVtjqXfeO2rGcZULQHaHI&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.S6XfC0biwarL3edwCziiTAAAAA&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 1 ",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse1.mm.bing.net/th?id=OIP.38i2WDklDD3dI0irptXHkgEsDI&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.T0APCa_GVfDuMwclMwqqEAHaE9&pid=Api&P=0&h=180",
+            "https://tse1.mm.bing.net/th?id=OIP.YHDPq0djyMfPWiMidDs0GAHaD8&pid=Api&P=0&h=180",
+            "https://tse1.mm.bing.net/th?id=OIP._XxQIO0r5BrEupJmhK6bwgHaEo&pid=Api&P=0&h=180",
+            "https://tse1.explicit.bing.net/th?id=OIP.ibfh0q4osFz4pAxS0nS65QEsDI&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hote 2 ",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse4.mm.bing.net/th?id=OIP.4QJFS6XKd73hD54Fy8K4pQHaE6&pid=Api&P=0&h=180",
+            "https://cdn.trendir.com/wp-content/uploads/old/interiors/2016/02/11/the-most-incredible-pools-on-the-planet-14.jpg",
+            "https://tse4.mm.bing.net/th?id=OIP.2G9E4rdGuA8NOc5Eemw4FwHaE7&pid=Api&P=0&h=180",
+            "https://tse1.mm.bing.net/th?id=OIP.1wEkYJu-yV6lrCyvDbQHPwHaFY&pid=Api&P=0&h=180"
+        ],
+        price: "30,000",
+        date: "25-20 May",
+        title: "Hotel 3",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://2.bp.blogspot.com/-Zbi8ERsnuUk/Uqi2pCtSXUI/AAAAAAAAdT8/N_d8sCAY_Ak/s1600/20_Most_Amazing_Swimming_Pools_Ever_on_world_of_architecture_10.jpg",
+            "https://i.pinimg.com/originals/19/e8/a7/19e8a79664752cb9562bb0c0d8a67290.jpg",
+            "https://i1.wp.com/theluxurytravelexpert.com/wp-content/uploads/2018/09/header.jpg?fit=1300%2C731&ssl=1",
+            "http://homesfeed.com/wp-content/uploads/2015/06/best-backyard-pools-with-amazing-lighting-and-cool-stone-around-the-poo-and-small-jacuzzy-and-living-space-area-and-mini-water-fountain.jpg",
+            "http://www.thewowstyle.com/wp-content/uploads/2015/01/pool-ideas-indoor-swimming-pool-designs-with-fantasy-indoor-swimming-pool-awesome-best-indoor-pools.jpg"
+        ],
+        price: "45,000",
+        date: "1-20 May",
+        title: "Hotel 4",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://cdn.shopify.com/s/files/1/0673/5777/files/tbart-768x512.jpg?v=1497565965",
+            "https://tse1.mm.bing.net/th?id=OIP.K5sefk7eJRAjwIK10NCIrQHaFj&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.9mDPVZZkpnYPp2eSXz9OKgHaFA&pid=Api&P=0&h=180",
+
+        ],
+        price: "20,000",
+        date: "10-20 Oct",
+        title: "Hotel 5",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "http://tryingtobalancethemadness.files.wordpress.com/2012/07/sky-pools-26205.jpg",
+            "https://i.pinimg.com/originals/0b/9d/4e/0b9d4e3f03c34968b6174f848fdd9dd5.jpg",
+            "https://cdn.trendir.com/wp-content/uploads/old/house-design/2016/01/08/glass-walled-swimming-pool-2.jpg",
+            "https://piximus.net/media/3667/amazing-pools-20.jpg",
+            "https://www.travelplusstyle.com/wp-content/uploads/2016/01/sonevajani-1880.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 6",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+
+            "https://tse1.mm.bing.net/th?id=OIP.XJok2mb5kW44ju9GqPah-wHaFj&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.j_fdpwAMj8bv9SZSdli_sgHaEK&pid=Api&P=0&h=180",
+            "https://tse2.mm.bing.net/th?id=OIP.-q0EjbD32-Rnuqz0awE97QHaE2&pid=Api&P=0&h=180",
+            "https://www.travelplusstyle.com/wp-content/uploads/2016/01/sonevajani-1880.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 7",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse1.mm.bing.net/th?id=OIP.-fcNuoPoiDEd7BCGGR7LkgAAAA&pid=Api&P=0&h=180",
+            "https://i.pinimg.com/originals/f5/b5/bc/f5b5bc4e4526c8ba936932ec41eaac11.jpg",
+            "https://static2.mansionglobal.com/production/media/article-images/2c6b1df3c3bbad5101864113280dd259/large_AmazingPools_Lead_SP.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 8",
+    },
+
+];
+
+
+export const list2 = [
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse1.mm.bing.net/th?id=OIP.zOovoBpSNyADCnJ-zeDm8QHaFj&pid=Api&P=0&h=180",
+            "https://assets.atdw-online.com.au/images/Original__9636530_EJ53_DSC_1465_2a5vw99.jpg?rect=240",
+            "https://i.pinimg.com/originals/4f/6b/2b/4f6b2b703523e28de9921f2b44cf5271.jpg",
+            "https://tse3.mm.bing.net/th?id=OIP.CJdrwm2PEKjQ-cgd8FJL3wHaEo&pid=Api&P=0&h=180",
+            "https://tse4.mm.bing.net/th?id=OIP.frFOAzRa2nWz6koy-OYe3wHaE8&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 1",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://i.pinimg.com/originals/4d/52/f1/4d52f12004e762be27c612018fa5ad7a.jpg",
+            "https://tse4.mm.bing.net/th?id=OIP.-vzLCVoUpNbELGXCRKRhqAHaFj&pid=Api&P=0&h=180",
+            "https://farmhacker.com/wp-content/uploads/2020/03/dirt-road-between-fences-and-beautiful-sunset-farm-background-wallpaper-pictures.jpg",
+            "https://images.pexels.com/photos/301010/pexels-photo-301010.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+        ],
+        price: "30,000",
+        date: "25-20 May",
+        title: "Hotel 2",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse4.mm.bing.net/th?id=OIP.cW2abPDSW2dnhzgCNoA3WAHaEO&pid=Api&P=0&h=180",
+            "https://i.pinimg.com/736x/1b/96/3d/1b963d145fda3d7c7c353635db9901b0.jpg",
+            "http://cdn.onlyinyourstate.com/wp-content/uploads/2016/02/Screen-Shot-2016-02-06-at-6.12.43-PM-700x447.png",
+            "https://getwallpapers.com/wallpaper/full/4/c/f/440670.jpg",
+            "https://tse1.mm.bing.net/th?id=OIP.85bCFv7ewmemwsb59uYnegAAAA&pid=Api&P=0&h=180"
+        ],
+        price: "45,000",
+        date: "1-20 May",
+        title: "Hotel 3",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse4.mm.bing.net/th?id=OIP.W6Qs2kFx7kaB-436T2BGQQHaE9&pid=Api&P=0&h=180",
+            "https://cdn.wallpapersafari.com/69/33/YiNayj.jpg",
+            "https://tse2.mm.bing.net/th?id=OIP.vZGh_oP9wWxTagwyShUlUQHaFj&pid=Api&P=0&h=180",
+
+        ],
+        price: "20,000",
+        date: "10-20 Oct",
+        title: "Hotel 4",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://s3.amazonaws.com/nichesites-files/sandboxworlds_com/images/surveys/127/3.jpg",
+            "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2015/8/20/1/William-C-Johnson_Ranch-to-Farmhouse-Reno_1.jpg.rend.hgtvcom.966.644.suffix/1440097264008.jpeg",
+            "https://tse4.mm.bing.net/th?id=OIP.GmcqCVa4PNB6T-q1mtAaugHaFJ&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.b8oZNE3nBYp_IIsVIea3VQHaE-&pid=Api&P=0&h=180",
+            "https://i.pinimg.com/originals/e6/c2/67/e6c267edcbff34ff3da7f82d484f68de.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 5",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+
+            "https://tse2.mm.bing.net/th?id=OIP.TavRduf729n4AOO-F9Y6mQHaEK&pid=Api&P=0&h=180",
+            "https://tse2.mm.bing.net/th?id=OIP.KULraCj3_sxsltBlryX_3AHaDf&pid=Api&P=0&h=180",
+            "https://static3.bigstockphoto.com/1/2/2/large1500/2212162.jpg",
+            "https://eventective-media.azureedge.net/1742764_md.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 6",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse3.mm.bing.net/th?id=OIP.C2JcMsGw5fMJQfnJTJ5JwgHaE8&pid=Api&P=0&h=180",
+            "https://1.bp.blogspot.com/-gfeT_3NIv_s/YFkBfawCtbI/AAAAAAAAGXA/1bC7ITNu2fY1ZX9I1W8KM0xObTa8KdgBQCLcBGAsYHQ/s750/WISATA.jpg",
+            "https://tse2.mm.bing.net/th?id=OIP.j0RLON4-lB0-Xw-VZcqGVgHaEJ&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 7",
+    },
+
+];
+
+
+
+
+export const list3 = [
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse1.mm.bing.net/th?id=OIP.zOovoBpSNyADCnJ-zeDm8QHaFj&pid=Api&P=0&h=180",
+            "https://assets.atdw-online.com.au/images/Original__9636530_EJ53_DSC_1465_2a5vw99.jpg?rect=240",
+            "https://i.pinimg.com/originals/4f/6b/2b/4f6b2b703523e28de9921f2b44cf5271.jpg",
+            "https://tse3.mm.bing.net/th?id=OIP.CJdrwm2PEKjQ-cgd8FJL3wHaEo&pid=Api&P=0&h=180",
+            "https://tse4.mm.bing.net/th?id=OIP.frFOAzRa2nWz6koy-OYe3wHaE8&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 1",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://i.pinimg.com/originals/4d/52/f1/4d52f12004e762be27c612018fa5ad7a.jpg",
+            "https://tse4.mm.bing.net/th?id=OIP.-vzLCVoUpNbELGXCRKRhqAHaFj&pid=Api&P=0&h=180",
+            "https://farmhacker.com/wp-content/uploads/2020/03/dirt-road-between-fences-and-beautiful-sunset-farm-background-wallpaper-pictures.jpg",
+            "https://images.pexels.com/photos/301010/pexels-photo-301010.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+        ],
+        price: "30,000",
+        date: "25-20 May",
+        title: "Hotel 2",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse4.mm.bing.net/th?id=OIP.cW2abPDSW2dnhzgCNoA3WAHaEO&pid=Api&P=0&h=180",
+            "https://i.pinimg.com/736x/1b/96/3d/1b963d145fda3d7c7c353635db9901b0.jpg",
+            "http://cdn.onlyinyourstate.com/wp-content/uploads/2016/02/Screen-Shot-2016-02-06-at-6.12.43-PM-700x447.png",
+            "https://getwallpapers.com/wallpaper/full/4/c/f/440670.jpg",
+            "https://tse1.mm.bing.net/th?id=OIP.85bCFv7ewmemwsb59uYnegAAAA&pid=Api&P=0&h=180"
+        ],
+        price: "45,000",
+        date: "1-20 May",
+        title: "Hotel 3",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse4.mm.bing.net/th?id=OIP.W6Qs2kFx7kaB-436T2BGQQHaE9&pid=Api&P=0&h=180",
+            "https://cdn.wallpapersafari.com/69/33/YiNayj.jpg",
+            "https://tse2.mm.bing.net/th?id=OIP.vZGh_oP9wWxTagwyShUlUQHaFj&pid=Api&P=0&h=180",
+
+        ],
+        price: "20,000",
+        date: "10-20 Oct",
+        title: "Hotel 4",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://s3.amazonaws.com/nichesites-files/sandboxworlds_com/images/surveys/127/3.jpg",
+            "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2015/8/20/1/William-C-Johnson_Ranch-to-Farmhouse-Reno_1.jpg.rend.hgtvcom.966.644.suffix/1440097264008.jpeg",
+            "https://tse4.mm.bing.net/th?id=OIP.GmcqCVa4PNB6T-q1mtAaugHaFJ&pid=Api&P=0&h=180",
+            "https://tse3.mm.bing.net/th?id=OIP.b8oZNE3nBYp_IIsVIea3VQHaE-&pid=Api&P=0&h=180",
+            "https://i.pinimg.com/originals/e6/c2/67/e6c267edcbff34ff3da7f82d484f68de.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 5",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+
+            "https://tse2.mm.bing.net/th?id=OIP.TavRduf729n4AOO-F9Y6mQHaEK&pid=Api&P=0&h=180",
+            "https://tse2.mm.bing.net/th?id=OIP.KULraCj3_sxsltBlryX_3AHaDf&pid=Api&P=0&h=180",
+            "https://static3.bigstockphoto.com/1/2/2/large1500/2212162.jpg",
+            "https://eventective-media.azureedge.net/1742764_md.jpg"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 6",
+    },
+    {
+        rating: "4",
+        desc: "Beach and sunset views",
+        imgSrc: [
+            "https://tse3.mm.bing.net/th?id=OIP.C2JcMsGw5fMJQfnJTJ5JwgHaE8&pid=Api&P=0&h=180",
+            "https://1.bp.blogspot.com/-gfeT_3NIv_s/YFkBfawCtbI/AAAAAAAAGXA/1bC7ITNu2fY1ZX9I1W8KM0xObTa8KdgBQCLcBGAsYHQ/s750/WISATA.jpg",
+            "https://tse2.mm.bing.net/th?id=OIP.j0RLON4-lB0-Xw-VZcqGVgHaEJ&pid=Api&P=0&h=180"
+        ],
+        price: "20,000",
+        date: "15-20 May",
+        title: "Hotel 7",
+    },
+
+];
+
---
+++ src/assets/images.links.js
@@ -0,0 +1,131 @@
+export const links = [
+   {
+        label: "Amazing pools",
+        imgSrc:  "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+         },
+    {
+        label: "Farms",
+        imgSrc:   "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+          },
+
+    {
+        label: "Amazing views",
+        imgSrc:"https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+        },
+    {
+        label: "Bed&breakfasts",
+        imgSrc:"https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+    
+        },
+    {
+        label: "Mansions",
+        imgSrc:  "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
+        },
+    {
+        label: "Lakefront",
+        imgSrc:"https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg",
+        },
+    {
+        label: "Rooms",
+        imgSrc:"https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+         },
+    {
+        label: "OMG!",
+        imgSrc:"https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+        },
+    {
+        label: "Beachfront",
+        imgSrc:"https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+         },
+    {
+        label: "Countryside",
+        imgSrc:"https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
+   
+         },
+    {
+        label: "Cabins",
+        imgSrc: "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
+   
+         },
+    {
+        label: "Tropical",
+        imgSrc:"https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+        },
+    {
+        label: "Tiny homes",
+        imgSrc:"https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg",
+        },
+    {
+        label: "Luxe",
+        imgSrc: "https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg",
+        },
+    {
+        label: "Farms",
+        imgSrc:"https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+         },
+
+    {
+        label: "Amazing views",
+        imgSrc:"https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+         },
+    {
+        label: "Bed & breakfasts",
+         imgSrc:"https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+        },
+    {
+        label: "Mansions",
+         imgSrc:"https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
+         },
+    {
+        label: "Lakefront",
+         imgSrc:"https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg",
+    
+        },
+    {
+        label: "Rooms",
+         imgSrc:"https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+            },
+   
+   ];
+      
+   
+      
+  
+      
+    
+       
+      
+    
+       
+    
+
+   
+      
+    
+      
+   
+        
+      
+      
+    
+      
+    
+       
+
+    
+       
+   
+        
+   
+        
+    
+      
+   
+      
+         
+
+
+
+
+
+
